3e9a4581479945b998300525e1219eb97e2488ea
Benjamin Albrecht <git@albrecht.io>
2015-04-24 19:57:40 +0000

    Simplify bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 ln -sf ~/.homesick/repos/dotmr/home/.mrconfig ~/.mrconfig
-# change git remote url to ssh
-cd ~/.homesick/repos/dotmr
-REMOTE=$(git remote -v|head -n 1|awk '{print $2}')
-test  "${REMOTE#*@}" != "$REMOTE" || \
-  git remote set-url origin $(echo $REMOTE|sed -e 's#.*://\([^/]*\)/#git@\1:#')
+cd
+mr checkout
 
